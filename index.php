e09a02d316af489c36e81736da3feeb60a64b764
Dami Ojo <oeo3@cornell.edu>
2017-04-19 12:42:15 -0400

    Index page
    Setting up the page to show on GitHub

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <title>AAMO</title>
+    </head>
+    <body>
+        <p>This is our site</p>
+    </body>
+</html>
