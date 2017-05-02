67af468d09436bbbf27c6586de49f6b212ec61ed
Dami Ojo <oeo3@cornell.edu>
2017-05-02 14:33:46 -0400

    fixed the icon images
    fixed the icon pathway for the welcome page

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
                     <h1>Conceptual. Ethical. Sustainable. Handcrafted.</h1>
                     <a class="enter_button" href="pages/home.php">Shop Now</a>
                    <div id="welcome_socialmedia">
-                        <a href="https://www.instagram.com/aamo_nepal/?hl=en" target="_blank"><img class="instagram" src="../img/socialmediaicon/insta.png"></a>
-                        <a href="https://www.facebook.com/AAMObyAayushaShrestha/" target="_blank" ><img class="facebook" src="../img/socialmediaicon/face.png"></a>
+                        <a href="https://www.instagram.com/aamo_nepal/?hl=en" target="_blank"><img class="instagram" src="img/socialmediaicon/insta.png"></a>
+                        <a href="https://www.facebook.com/AAMObyAayushaShrestha/" target="_blank" ><img class="facebook" src="img/socialmediaicon/face.png"></a>
                     </div>
                 </div>
             </div>
