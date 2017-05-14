1cb2273ab260fb9d7b4ea05970d76f37dbcf682c
Dami Ojo <oeo3@cornell.edu>
2017-05-14 19:54:02 -0400

    Changes made for the products page menu bar
    Pages: item menu functional( accordian style)
    
    CSS: css for the menu items

--- pages/products.php
+++ pages/products.php
@@ -38,40 +38,40 @@
                                 }
                             }
 		              </script></li>
-                    <li><a href="fav.php">FAV</li>
+                    <li><a href="fav.php">FAV</a></li>
                 </ul>
             </div>
             <div id="productscontent">
-            <div id="product_menu">
-                <h1>Categories</h1>
-                <ul>
-                    <li><a id="cat_jewel"> JEWELERY</a></li>
-                    <li><a id="cat_wood">WOOD WORK</a></li>
-                    <li><a id="cat_textiles">TEXTILES</a></li>
-                </ul>
-                <!--Note: category items: on click, sql code will search the database under the product in album table and show items which have the same value as the clicked filter-->
-                <h2>Filters</h2>
-                <ul class="filters">
-                    <script type="text/javascript">
-                        $(document).ready(function(){
-                           $(".filters").click(function(){
-                              $(".filters ul").slideDown(); 
-                               //write code to slide content back up: if content is showing under ".filters ul", when ".filters ul" is clicked, sub content disappears. 
-                                //if($(this).prev().is(".visible")){$(this).prev().slideUp();}
-                           }); 
+                <script type="text/javascript">
+                    $(document).ready(function(){
+                        $("#product_menu h1").click(function(){
+                            //slide up all the link lists
+                            $("#product_menu ul ul").slideUp();
+                            //slide down the link list below h1 clicked - only if closed
+                            if(!$(this).next().is(":visible")){
+                               $(this).next().slideDown();
+                            }
                         });
-                    </script>
-                    <li><a href="#" >Series<span class="sub_arrow"></span></a>
-                        <ul class="filter_sub">
-                           <li><a href="#">Garima</a></li>
-                            <li><a href="#">Lo</a></li>
-                            <li><a href="#">Shakti</a></li>
+                    });
+                </script>
+            <div id="product_menu">
+                <ul>
+                    <li class="active">
+                        <h1>Categories</h1>
+                        <ul>
+                            <li><a href="#">Jewelry</a></li>
+                            <li><a href="#">Woodwork</a></li>
+                            <li><a href="#">Textile</a></li>
                         </ul>
                     </li>
-                    <!--Note: sub menu items in the filter section: on click, sql code will search the database under the product table and show items which have the same value as the clicked filter-->
-                    
-                    <li><a href="#">Material</a></li>
-                </ul>
+                    <li>
+                        <h1>Filters</h1>
+                        <ul>
+                            <li><a href="#">Series</a></li>
+                            <li><a href="#">Material</a></li>
+                        </ul>
+                    </li>
+                </ul>    
             </div>
             
             <div class="gallery">
